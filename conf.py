040807d77a4c693a7603cc053c1fe046fc59ad7c
youngwildthought <68626438+youngwildthought@users.noreply.github.com>
2020-07-23 10:32:29 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -14,8 +14,8 @@ template = {
     "path": "../Galileo"
 }
 enable_jsdelivr = {
-    "enabled": False,
-    "repo": ""
+    "enabled": True,
+    "repo": "youngwildthought.github.io/Blog-With-GitHub-Boilerplate@gu-pages"
 }
 
 # 站点设置
