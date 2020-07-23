645e2d2ccb2c19da48566abc357755b474326cae
youngwildthought <68626438+youngwildthought@users.noreply.github.com>
2020-07-23 10:32:58 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -15,7 +15,7 @@ template = {
 }
 enable_jsdelivr = {
     "enabled": True,
-    "repo": "youngwildthought.github.io/Blog-With-GitHub-Boilerplate@gu-pages"
+    "repo": "youngwildthought.github.io/Blog-With-GitHub-Boilerplate@gh-pages"
 }
 
 # 站点设置
