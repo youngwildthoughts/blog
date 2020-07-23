b29beafc1ae915c0f037c81148662b145b5f56f7
youngwildthought <68626438+youngwildthought@users.noreply.github.com>
2020-07-23 10:23:25 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -19,13 +19,13 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "少年奇思录"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
+author = "卓彦"
+email = "xiezy.thomas@foxmail.com"
 author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
+description = "旅程本身即是奖赏!"
 key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
